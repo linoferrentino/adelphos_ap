9877156b603e574f3e68d15ab526e49dddfc33d1
Lino Ferrentino <lino.ferrentino@gmail.com>
2026-01-24 16:25:51 +0000

    Created the parser for the adelphos uri, not tested.

--- app/dao/AdelphosUri.py
+++ app/dao/AdelphosUri.py
@@ -16,6 +16,11 @@
 # The URIs are used to identify objects in the federated database
 
 from enum import StrEnum
+from dataclasses import dataclass
+import dataclasses
+import re
+
+from app.api.AdelphosException import AdelphosException
 
 
 # the format of an adelphos URI is dependent on the type
@@ -36,12 +41,19 @@ from enum import StrEnum
 # ##alias.family@host in human form
 # or
 # ##$392@host in mechanical form
-# 
-# the single @ is for a local alias
-# alias
 
-# this is a currency
+# we might have also the alternative representation
+# #al#alias.family@host
+# or
+# #al#$<ID>@host
+# 
+# the single string is for a local alias
+# ##alias.family
+
+# this is a currency in the fediverse
 # #cu#euro@www.adelphos.it
+
+# #cu#euro for a local currency
 
 # this is a group
 # #gr#terrible_cats@www.adelphos.it
@@ -59,12 +71,9 @@ from enum import StrEnum
 # #ob#used_pc99@www.adelphos.it
 
 
-
-
-
 # This enumeration will list all the types in the federated database
 # with their compact representation (two letters).
-class EAdelphosTypes(StrEnum):
+class EAdelphosType(StrEnum):
     ALIAS_TYPE = 'al'
     CHEQUE_TYPE = 'cq'
     CURRENCY_TYPE = 'cu'
@@ -72,17 +81,147 @@ class EAdelphosTypes(StrEnum):
     GROUP_TYPE = 'gr'
     OBJECT_TYPE = 'ob'
     PLACE_TYPE = 'pl'
+    TRANSIT_LINE = 'tr'
     TRUST_LINE = 'tl'
 
 
-# this function will parse an URI in adelphos and return the
-# parsed object.
-def uriparse(uri):
-    pass
-
-
+# this is a frozen dataclass
+@dataclass
 class AdelphosUri:
 
-    def __init__(self):
-        pass
+    obj_type: EAdelphosType
+    is_numeric: bool
+    host_name: str
+
+    _ : dataclasses.KW_ONLY
+
+    name: str = None
+    # Only for the aliases we have a family
+    family: str = None
+    numeric_id: int = None
+
+
+
+# to divide I have to look for a point
+def _divide_uri_alias_family(alias_family):
+
+    alias_family_splits = alias_family.split(".")
+    if (len(alias_family_splits) != 2):
+        raise AdelphosException(f"Illegal alias_family {alias_family}. I \
+need a full stop between alias and family")
+    
+    return alias_family_splits
+
+
+def _parse_object_part(object_part):
+    # I have to know if this is in mechanical or human form
+    if (object_part[0] == '$'):
+        # this is a mechanical form, so we have to remove the
+        # dollar and see the integer inside.
+        object_part = alias_family[1:]
+
+        # maybe there are extraneous characters.
+        if (re.match(r"\D", alias_family) is not None):
+            raise AdelphosException(f"Illegal numeric id {alias_family}")
+
+        mechanical_id = int(alias_family)
+
+        return (None, None, mechanical_id)
+
+     # this is human form, so we simply divide in alias and family
+     (alias, family) = _divide_uri_alias_family(object_part)
+     return (alias, family, None)
+
+
+def _parse_uri_type(uri_type_str):
+
+    match uri_type_str:
+        case 'al':
+            uri_type = EAdelphosType.ALIAS_TYPE
+        case 'cq':
+            uri_type = EAdelphosType.CHEQUE_TYPE
+        case 'cu':
+            uri_type = EAdelphosType.CURRENCY_TYPE
+        case 'fa':
+            uri_type = EAdelphosType.FAMILY_TYPE
+        case 'gr':
+            uri_type = EAdelphosType.GROUP_TYPE
+        case 'ob':
+            uri_type = EAdelphosType.OBJECT_TYPE
+        case 'pl':
+            uri_type = EAdelphosType.PLACE_TYPE
+        case 'tr':
+            uri_type = EAdelphosType.TRANSIT_LINE
+        case 'tl':
+            uri_type = EAdelphosType.TRUST_LINE
+        case _:
+            raise AdelphosException(f"Unknown type \
+{type_name_match.group(1)}")
+
+    return uri_type
+
+
+# this function will parse an URI in adelphos and return the
+# parsed object
+# there is not much error handling (yet)
+def uriparse(uri):
+
+    # first of all I need to know if this uri is local or remote.
+    local_remote_splits = uri.split('@')
+    if (len(local_remote_splits) > 2):
+        raise AdelphosException(f"Illegal adelphos uri {uri} \
+more than one '@'")
+        
+    if (len(local_remote_splits) == 2):
+        host_part = local_remote_splits[1]
+    else:
+        host_part = None
+
+    object_part = local_remote_splits[0]
+
+    #Now we have to divide the object part.
+    # first of all is it an alias?
+    alias_match = re.match(r"##(.*)", object_part)
+    if (alias_match is not None):
+
+        uri_type = EAdelphosType.ALIAS_TYPE
+        (name, family, mechanical_id) = _parse_object_part(
+            alias_match.group(1))
+        
+    else:
+
+        # this is an object, which has not a family
+
+        # first of all we derive the type
+        type_name_match = re.match(
+r"#([a-z0-9\.-_]{2}#([^#]*)$", object_part)
+
+        if (type_name_match is None):
+            raise AdelphosException(f"Illegal URI {object_part} \
+I was expecting something like #<type>#<name or id>")
+
+        uri_type = _parse_uri_type(type_name_match.group(1))
+
+        (name, family, mechanical_id) = _parse_object_part(
+                type_name_match.group(2))
+
+        if (uri_type != EAdelphosType.ALIAS_TYPE):
+            if (family is not None):
+                raise AdelphosException(
+f"Illegal identifier {type_name_match.group(2)}: \
+Only aliases can have a family.")
+
+
+    # Ok now we can create the object
+    if (mechanical_id is not None):
+        adelphos_uri = AdelphosUri(uri_type,
+                        True, host_part, numeric_id = mechanical_id)
+    else:
+        adelphos_uri = AdelphosUri(uri_type,
+                        False, host_part, name = name, family = family)
+
+    # return the parsed object
+    return adelphos_uri
+
+
 
